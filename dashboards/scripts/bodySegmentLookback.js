2880d638a8d97a8770b3bf8638777749ecf091fc
Jason Chiu <jforeza@gmail.com>
2022-11-06 09:25:36 -0800

    Enforce rows/cols for more responsive

--- dashboards/scripts/bodySegmentLookback.js
+++ dashboards/scripts/bodySegmentLookback.js
@@ -4,38 +4,33 @@ let segmentLb = document.createElement("div");
 segmentLb.classList.add("segment-lookback");
 segmentLb.innerHTML =
   `
-    <div class="container">
-        <h1><span class="loc-a-name"></span> to <span class="loc-b-name"></span></h1>
-        <div class="section">
-          <h2>Data Insights for last 30 days</h2>
-          <div id="a-b-insight-dynamic">
-          </div>
-        </div>
-        <div class="divider"></div>
-        <div class="section">
-          <h2>Trip Data for last 30 Days (scatter)</h2>
-          <div id="monthlyLookBack_AToB" class="section">
-          </div>
-        </div>
-  
+  <div class="container">
+  <h2><span class="loc-a-name"></span> to <span class="loc-b-name"></span></h2>
+  <div class="row">
+    <div class="col m6 s12 section">
+      <h3>Data Insights for last 30 days</h3>
+      <div id="a-b-insight-dynamic">
       </div>
-  
-      <div class="container">
-        <h1><span class="loc-b-name"></span> to <span class="loc-a-name"></span></h1>
-  
-        <div class="section">
-          <h2>Data Insights for last 30 days</h2>
-          <div id="b-a-insight-dynamic">
-          </div>
-        </div>
-  
-        <div class="divider"></div>
-        <div class="section">
-          <h2>Trip Data for last 30 Days (scatter)</h2>
-          <div id="monthlyLookBack_BToA" class="section">
-          </div>
-        </div>
-
+    </div>
+    <div class="col m6 s12 section">
+      <h3>Trip Data last 30 Days</h3>
+      <div id="monthlyLookBack_AToB" class="section">
       </div>
+    </div>
+  </div>
+  <div class="row">
+    <h2><span class="loc-b-name"></span> to <span class="loc-a-name"></span></h2>
+    <div class="col m6 s12 section">
+      <h3>Data Insights for last 30 days</h3>
+      <div id="b-a-insight-dynamic">
+      </div>
+    </div>
+    <div class="col m6 s12 section">
+      <h3>Trip Data last 30 Days</h3>
+      <div id="monthlyLookBack_BToA" class="section">
+      </div>
+    </div>
+  </div>
+</div>
 `
 document.getElementsByTagName("main")[0].append(segmentLb);
